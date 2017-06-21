72d4ff40a962718a791b5f0906553cfecd7d5330
Ronald Tol <tol.ronald@gmail.com>
2017-06-21 18:14:59 +0200

    -

--- database/migrations/2017_06_15_091354_create_date_module_table.php
+++ database/migrations/2017_06_15_091354_create_date_module_table.php
@@ -2,7 +2,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-class CreateOrderProductTable extends Migration
+class CreateDateModuleTable extends Migration
 {
     /**
      * Run the migrations.
